ff238028549bb7b08c6e39ec4587d58ef546f707
carol-evolvier-intern <carolevolvier@gmail.com>
2020-11-04 17:11:31 +0530

    Events added

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import AddEvent from './AddEvent';
+// we import withTracker and Events into our app file
+import { withTracker } from 'meteor/react-meteor-data';
+import { Events } from "../api/events";
 
-class Todo extends Component {
+// Create a new React Component `EventApp`
+class EventApp extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <AddEvent />
+        <pre>DB Stuff: {JSON.stringify(this.props, null, ' ')} </pre>
       </div>
     );
   }
 }
 
-export default Todo;
+// Wrap `EventApp` with the HOC withTracker and call the new component we get `App`
+const App = withTracker(() => {
+  return {
+    events: Events.find({}). fetch()
+  }
+})(EventApp);
+
+// export the component `App`
+export default App;
